dc10f1ebe09b849a3819a97e3892819d50725bb1
tsah012 <tsah012@gmail.com>
2023-03-06 21:00:47 +0200

    replace mongodb lib with mongoose lib
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- custom_modules/DAL/logs/index.js
+++ custom_modules/DAL/logs/index.js
@@ -1,14 +1,11 @@
-const mongo = require('../../mongo');
-const logsColl = require('../../configuration/app').logsCollection;
+const logModel = require('./logModel');
 
 
 module.exports.getLogs = async function () {
     try {
-        const db = mongo.getDB();
-        const data = await db.collection(logsColl).find().toArray();
+        const data = logModel.find();
         return data;
     } catch (error) {
         throw (error);
     }
-
 }
